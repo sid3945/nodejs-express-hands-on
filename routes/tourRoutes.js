4207e2288d0433c912fc3e4ec61c96fc62425a3b
sid3934 <siddharth.su@somaiya.edu>
2024-05-06 01:04:09 +0530

    route handlers/ controllers separated into a separate file

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,85 +1,19 @@
 const express = require('express');
 const fs = require('fs');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router(); //creating a router middleware
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/tours.json`, 'utf-8'));
-
-// Tour handlers
-getTour = (req, res) => {
-    const id = parseInt(req.params.id);
-    const tour = tours.find(el => el.id === id);
-
-    if(id > tours.length || !tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: tour
-        }
-    });
-};
-
-updateTour = (req, res) => {
-    if(req.params.id > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated tour'
-        }
-    });
-};
-
-deleteTour = (req, res) => {
-    // Implement your delete logic here
-};
-
-getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tours: tours
-        }
-    });
-};
-
-createTour = (req, res) => {
-    const newId = tours[tours.length-1].id + 1;
-    const newTour = {
-        id: newId,
-        ...req.body
-    };
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data.json`, JSON.stringify(tours), err =>{
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tours: "updated the new tour"
-            }
-        });
-    });
-};
-
 router
     .route('/')
-    .get(getAllTours)
-    .post(createTour);
+    .get(tourController.getAllTours)
+    .post(tourController.createTour);
 //we write / because we are already in the /api/v1/tours route as router middleware is mounted on it
 router
     .route('/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,80 +1,17 @@
 const express = require('express');
+const userController = require('../controllers/userController');
+
 router = express.Router();
-
-// User handlers
-getUser = (req, res) => {
-    const id = parseInt(req.params.id);
-    const user = users.find(el => el.id === id);
-
-    if(id > users.length || !user){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: user
-        }
-    });
-};
-
-updateUser = (req, res) => {
-    if(req.params.id > users.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: 'Updated user'
-        }
-    });
-};
-
-deleteUser = (req, res) => {
-    // Implement your delete logic here
-};
-
-getAllUsers = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            users: users
-        }
-    });
-};
-
-createUser = (req, res) => {
-    const newId = users[users.length-1].id + 1;
-    const newUser = {
-        id: newId,
-        ...req.body
-    };
-    users.push(newUser);
-    fs.writeFile(`${__dirname}/dev-data/data.json`, JSON.stringify(users), err =>{
-        res.status(201).json({
-            status: 'success',
-            data: {
-                users: "updated the new user"
-            }
-        });
-    });
-};
 
 router
     .route('/')
-    .get(getAllUsers)
-    .post(createUser);
+    .get(userController.getAllUsers)
+    .post(userController.createUser);
 
 router
     .route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
 
 module.exports = router;
