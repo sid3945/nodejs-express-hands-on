dcd5880dec2c01bebd3188e054fd57390fe11190
sid3934 <siddharth.su@somaiya.edu>
2024-05-12 14:40:35 +0530

    updated with route param to validate ID

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -65,3 +65,14 @@ exports.createTour = (req, res) => {
         });
     });
 };
+
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if(val > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,9 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router(); //creating a router middleware
 
+router.param('id', tourController.checkID); //middleware to check if the id is valid or not before executing the route handler
+
+/**so .param is the function provided by Express where you can access a certain parameter persent in the route and run  middleware only when that param is found */
 router
     .route('/')
     .get(tourController.getAllTours)
