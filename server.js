df0d28a051859f34d91284bd06e91d12563be7d1
sid3934 <siddharth.su@somaiya.edu>
2024-05-20 00:06:24 +0530

    introduction to mongoose schema and model

--- server.js
+++ server.js
@@ -11,6 +11,31 @@ mongoose.connect(DB, {
     console.log("Database connection successful!");
 });
 
+//mongoose is an Object Data Modelling (ODM) library 
+
+//mongoose is all about models, to create a document in the collection, we need to create a model and model is created using schema
+//A schema in Mongoose is a structure that defines the shape of the data in MongoDB. It specifies the fields available in the documents along with their types, default values, validators, etc.
+
+//A model, on the other hand, is a constructor compiled from the Schema definitions. An instance of a model represents a document in MongoDB. You can create, read, update, and delete documents in the database using models.
+const tourSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating:{
+        type: Number,
+        default: 4.5
+    },
+    price:{
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
