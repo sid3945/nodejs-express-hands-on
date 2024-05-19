977f371de041910b5658abeea7ef1dbc8b497af2
sid3934 <siddharth.su@somaiya.edu>
2024-05-19 23:25:46 +0530

    addition of mongoose and connection to db on a mongodb atlas cluster

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const dotenv = require('dotenv');
-
 dotenv.config({path: './config.env'});
+
+const mongoose = require('mongoose');
+const DB = process.env.DATABASEURL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con => {
+    console.log("Database connection successful!");
+});
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
